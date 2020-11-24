437782232abb37ff62456f2c2edeadbaab43cc14
me <codemonkey555@gmail.com>
2020-11-23 23:28:25 -0800

    Reworked host vm network config service to properly depend on the multipass service and to wait until that other service completes it's iptables config before making changes.

--- host_vm_network_service/host_vm_network_config.sh
+++ host_vm_network_service/host_vm_network_config.sh
@@ -1,15 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
-# get target rule details
-new_rule=$(iptables-legacy-save | grep -- ' FORWARD .* --ctstate RELATED,ESTABLISHED ' | cut -d' ' -f3- | sed -e 's/ RELATED/ NEW,RELATED/')
-matches=$(echo "$new_rule" | wc -l)
-if [ $matches -lt 1 ]; then
-    echo "$0: failed to discover FORWARD rule target" >&2
-    exit 2
-elif [ $matches -gt 1 ]; then
-    echo "$0: found too many matching FORWARD rule targets" >&2
+TIMEOUT=180
+
+# loop until we see expected rule
+start_time=$(/usr/bin/date +'%s')
+while true; do
+    # get target rule details
+    matched_rule=$(iptables-legacy-save | grep -E -- ' FORWARD .* --ctstate (NEW,)?RELATED,ESTABLISHED ')
+    if [ $? -eq 0 ]; then
+        # found a matching rule
+        break
+    fi
+    
+    cur_time=$(/usr/bin/date +'%s')
+    ((elapsed=$end_time - $cur_time))
+    if [ $elapsed -gt $TIMEOUT ]; then
+        echo "$0: timed out waiting to discover FORWARD rule target ($elapsed seconds elapsed)." >&2
+        exit 1
+    fi
+
+    sleep 1
+done
+matches=$(echo "$matched_rule" | wc -l)
+if [ $matches -gt 1 ]; then
+    echo "$0: found too many matching FORWARD rule targets ($matches)" >&2
     exit 3
 fi
+
+echo $matched_rule |grep -E -- ' FORWARD .* --ctstate NEW,'
+if [ $? -eq 0 ]; then
+    # rule already exists, nothing to do
+    exit 0
+fi
+
+new_rule=$(echo $matched_rule | cut -d' ' -f3- | sed -e 's/ RELATED/ NEW,RELATED/')
 
 # get target rule number
 output=$(iptables-legacy -t filter -L FORWARD --line-numbers| grep ' RELATED,ESTABLISHED')
